1710ffe328f58df85b4f1b354d8816080c9d0eda
limjongwoo <whddn528@naver.com>
2021-08-01 17:24:04 +0900

    Blocking MongoOperations add

--- src/main/java/me/jongwoo/springbootch1reactive/repository/BlockingItemRepository.java
+++
@@ -1,7 +0,0 @@
-package me.jongwoo.springbootch1reactive.repository;
-
-import me.jongwoo.springbootch1reactive.domain.Item;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BlockingItemRepository extends CrudRepository<Item, String> {
-}
--- src/main/java/me/jongwoo/springbootch1reactive/repository/TemplateDatabaseLoader.java
+++ src/main/java/me/jongwoo/springbootch1reactive/repository/TemplateDatabaseLoader.java
@@ -4,22 +4,19 @@ import me.jongwoo.springbootch1reactive.domain.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RepositoryDatabaseLoader implements ApplicationRunner {
+public class TemplateDatabaseLoader implements ApplicationRunner {
 
     @Autowired
-    BlockingItemRepository repository;
+    private MongoOperations mongo;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        initialize(repository);
-    }
-
-    private void initialize(BlockingItemRepository repository) {
-        repository.save(new Item("Alf alarm clock", 19.99));
-        repository.save(new Item("Smurf TV tray", 19.99));
+        mongo.save(new Item("Alf alarm clock", 19.99));
+        mongo.save(new Item("Smurf TV tray", 24.99));
     }
 }
