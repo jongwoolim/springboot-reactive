f4a08d33a155327eadb698d022c025cc6841935a
limjongwoo <whddn528@naver.com>
2021-08-03 10:38:22 +0900

    ItemRepository querymethod add

--- src/main/java/me/jongwoo/springbootch1reactive/domain/Item.java
+++ src/main/java/me/jongwoo/springbootch1reactive/domain/Item.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+import java.util.Date;
+
 @NoArgsConstructor
 @Data
 public class Item {
@@ -11,7 +13,12 @@ public class Item {
     @Id
     private String id;
     private String name;
+    private String description;
     private double price;
+    private String distributorRegion;
+    private Date releaseDate;
+    private int availableUnits;
+    private boolean active;
 
     public Item(String name, double price) {
         this.name = name;
--- src/main/java/me/jongwoo/springbootch1reactive/repository/ItemRepository.java
+++ src/main/java/me/jongwoo/springbootch1reactive/repository/ItemRepository.java
@@ -1,7 +1,11 @@
 package me.jongwoo.springbootch1reactive.repository;
 
 import me.jongwoo.springbootch1reactive.domain.Item;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
-public interface ItemRepository extends ReactiveCrudRepository<Item, String>{
+public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
+
+    Flux<Item> findByNameContaining(String partialName);
 }
