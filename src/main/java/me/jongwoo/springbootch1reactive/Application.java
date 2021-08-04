ccf8df0ced7c5b66c3a1829c6777a49cd8e5a4f8
limjongwoo <whddn528@naver.com>
2021-08-04 20:34:33 +0900

    blockhound add

--- src/main/java/me/jongwoo/springbootch1reactive/Application.java
+++ src/main/java/me/jongwoo/springbootch1reactive/Application.java
@@ -2,12 +2,18 @@ package me.jongwoo.springbootch1reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        // 해당 스레드가 블로킹 메소드 호출을 허용하는지 검사한다.
+        BlockHound.install();
+
         SpringApplication.run(Application.class, args);
     }
+
+
 
 }
