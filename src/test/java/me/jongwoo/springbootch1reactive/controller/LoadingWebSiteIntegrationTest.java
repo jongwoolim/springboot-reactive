c87f7f10d4ff0651af1f058f8c6742b8632c215d
limjongwoo <whddn528@naver.com>
2021-08-07 12:56:11 +0900

    web container test

---
+++ src/test/java/me/jongwoo/springbootch1reactive/controller/LoadingWebSiteIntegrationTest.java
@@ -0,0 +1,29 @@
+package me.jongwoo.springbootch1reactive.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class LoadingWebSiteIntegrationTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void test(){
+        client.get().uri("/").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class)
+                .consumeWith(exchangeResult -> {
+                    assertThat(exchangeResult.getResponseBody()).contains("<input type=\"submit\" value=\"Add to Cart\"/>");
+                });
+    }
+}
