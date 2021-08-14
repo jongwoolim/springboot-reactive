abf7203a9b7366d9c17c8dff525150465bf3190e
limjongwoo <whddn528@naver.com>
2021-08-14 20:23:42 +0900

    index. adoc add

--- src/test/java/me/jongwoo/springbootch1reactive/controller/ApiIItemControllerTest.java
+++ src/test/java/me/jongwoo/springbootch1reactive/controller/ApiIItemControllerTest.java
@@ -10,8 +10,10 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -29,6 +31,21 @@ class ApiIItemControllerTest {
 
     @MockBean
     private ItemRepository repository;
+
+    @Test
+    public void postNewIten(){
+        when(repository.save(any())).thenReturn(
+                Mono.just(new Item("1", "Alf alarm clock", "nothing important", 19.99)));
+
+        this.webTestClient.post().uri("/api/items")
+                .bodyValue(new Item("Alf alarm clock", "nothing important", 19.99))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .consumeWith(document("post-new-item", preprocessResponse(prettyPrint())));
+
+
+    }
 
     @Test
     public void findingAllItems(){
