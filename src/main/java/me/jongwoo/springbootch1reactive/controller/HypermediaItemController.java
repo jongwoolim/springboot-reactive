5bfd6545c8291cf20d092bf650c1c57f17da32b6
limjongwoo <whddn528@naver.com>
2021-08-14 23:17:45 +0900

    Hypermedia profile add

--- src/main/java/me/jongwoo/springbootch1reactive/controller/HypermediaItemController.java
+++ src/main/java/me/jongwoo/springbootch1reactive/controller/HypermediaItemController.java
@@ -4,11 +4,19 @@ import lombok.RequiredArgsConstructor;
 import me.jongwoo.springbootch1reactive.domain.Item;
 import me.jongwoo.springbootch1reactive.repository.ItemRepository;
 import org.springframework.hateoas.*;
+import org.springframework.hateoas.mediatype.alps.Alps;
+import org.springframework.hateoas.mediatype.alps.Type;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mediatype.alps.Alps.alps;
+import static org.springframework.hateoas.mediatype.alps.Alps.descriptor;
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
 
@@ -44,4 +52,21 @@ public class HypermediaItemController {
                         .map(selfLink -> CollectionModel.of(entityModels, selfLink)));
     }
     // end::find-all[]
+
+
+    @GetMapping(value = "/hypermedia/items/profile", produces = MediaTypes.ALPS_JSON_VALUE)
+    public Alps profile() {
+        return alps()
+                .descriptor(Collections.singletonList(descriptor()
+                        .id(Item.class.getSimpleName() + "-repr")
+                        .descriptor(Arrays.stream(
+                                Item.class.getDeclaredFields())
+                                .map(field -> descriptor()
+                                        .name(field.getName())
+                                        .type(Type.SEMANTIC)
+                                        .build())
+                                .collect(Collectors.toList()))
+                        .build()))
+                .build();
+    }
 }
