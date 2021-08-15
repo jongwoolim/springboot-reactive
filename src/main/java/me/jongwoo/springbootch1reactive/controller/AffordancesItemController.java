1a35b73e7c976b3cb9afb760e78b31277bffe797
limjongwoo <whddn528@naver.com>
2021-08-15 14:25:28 +0900

    AffordancesItemControllerTest add

--- src/main/java/me/jongwoo/springbootch1reactive/controller/AffordancesItemController.java
+++ src/main/java/me/jongwoo/springbootch1reactive/controller/AffordancesItemController.java
@@ -1,23 +1,32 @@
 package me.jongwoo.springbootch1reactive.controller;
 
-import lombok.RequiredArgsConstructor;
-import me.jongwoo.springbootch1reactive.domain.Item;
-import me.jongwoo.springbootch1reactive.repository.ItemRepository;
-import org.springframework.hateoas.*;
-import org.springframework.hateoas.mediatype.alps.Alps;
-import org.springframework.hateoas.mediatype.alps.Type;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
+import static org.springframework.hateoas.mediatype.alps.Alps.*;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.*;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
-import static org.springframework.hateoas.mediatype.alps.Alps.alps;
-import static org.springframework.hateoas.mediatype.alps.Alps.descriptor;
-import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
+import lombok.RequiredArgsConstructor;
+import me.jongwoo.springbootch1reactive.domain.Item;
+import me.jongwoo.springbootch1reactive.repository.ItemRepository;
+import reactor.core.publisher.Mono;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Links;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.mediatype.alps.Alps;
+import org.springframework.hateoas.mediatype.alps.Type;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,81 +34,72 @@ public class AffordancesItemController {
 
     private final ItemRepository repository;
 
-    // tag::root[]
     @GetMapping("/affordances")
-    Mono<RepresentationModel<?>> root() {
+    public Mono<RepresentationModel<?>> root() {
         AffordancesItemController controller = methodOn(AffordancesItemController.class);
 
-        Mono<Link> selfLink = linkTo(controller.root()) //
-                .withSelfRel() //
+        Mono<Link> selfLink = linkTo(controller.root())
+                .withSelfRel()
                 .toMono();
 
-        Mono<Link> itemsAggregateLink = linkTo(controller.findAll()) //
-                .withRel(IanaLinkRelations.ITEM) //
+        Mono<Link> itemsAggregateLink = linkTo(controller.findAll())
+                .withRel(IanaLinkRelations.ITEM)
                 .toMono();
 
-        return selfLink.zipWith(itemsAggregateLink) //
-                .map(links -> Links.of(links.getT1(), links.getT2())) //
+        return selfLink.zipWith(itemsAggregateLink)
+                .map(links -> Links.of(links.getT1(), links.getT2()))
                 .map(links -> new RepresentationModel<>(links.toList()));
     }
-    // end::root[]
 
-    // tag::find-all[]
     @GetMapping("/affordances/items")
-    Mono<CollectionModel<EntityModel<Item>>> findAll() {
+    public Mono<CollectionModel<EntityModel<Item>>> findAll() {
         AffordancesItemController controller = methodOn(AffordancesItemController.class);
 
-        Mono<Link> aggregateRoot = linkTo(controller.findAll()) //
-                .withSelfRel() //
-                .andAffordance(controller.addNewItem(null)) // <1>
+        Mono<Link> aggregateRoot = linkTo(controller.findAll())
+                .withSelfRel()
+                .andAffordance(controller.addNewItem(null))
                 .toMono();
 
-        return this.repository.findAll() // <2>
-                .flatMap(item -> findOne(item.getId())) // <3>
-                .collectList() // <4>
-                .flatMap(models -> aggregateRoot //
-                        .map(selfLink -> CollectionModel.of( //
-                                models, selfLink))); // <5>
+        return this.repository.findAll()
+                .flatMap(item -> findOne(item.getId()))
+                .collectList()
+                .flatMap(models -> aggregateRoot
+                        .map(selfLink -> CollectionModel.of(
+                                models, selfLink)));
     }
-    // end::find-all[]
-
-    // tag::find-one[]
-    @GetMapping("/affordances/items/{id}") // <1>
-    Mono<EntityModel<Item>> findOne(@PathVariable String id) {
-        AffordancesItemController controller = methodOn(AffordancesItemController.class); // <2>
-
-        Mono<Link> selfLink = linkTo(controller.findOne(id)) //
-                .withSelfRel() //
-                .andAffordance(controller.updateItem(null, id)) // <3>
-                .toMono();
-
-        Mono<Link> aggregateLink = linkTo(controller.findAll()) //
-                .withRel(IanaLinkRelations.ITEM) //
-                .toMono();
-
-        return Mono.zip(repository.findById(id), selfLink, aggregateLink) //
-                .map(o -> EntityModel.of(o.getT1(), Links.of(o.getT2(), o.getT3())));
-    }
-    // end::find-one[]
-
-    // tag::add-new-item[]
-    @PostMapping("/affordances/items") // <1>
-    Mono<ResponseEntity<?>> addNewItem(@RequestBody Mono<EntityModel<Item>> item) { // <2>
-        return item //
-                .map(EntityModel::getContent) // <3>
-                .flatMap(this.repository::save) // <4>
-                .map(Item::getId) // <5>
-                .flatMap(this::findOne) // <6>
-                .map(newModel -> ResponseEntity.created(newModel // <7>
-                        .getRequiredLink(IanaLinkRelations.SELF) //
-                        .toUri()).body(newModel.getContent()));
-    }
-    // end::add-new-item[]
 
     @GetMapping("/affordances/items/{id}")
+    public Mono<EntityModel<Item>> findOne(@PathVariable String id) {
+        AffordancesItemController controller = methodOn(AffordancesItemController.class);
+
+        Mono<Link> selfLink = linkTo(controller.findOne(id))
+                .withSelfRel()
+                .andAffordance(controller.updateItem(null, id))
+                .toMono();
+
+        Mono<Link> aggregateLink = linkTo(controller.findAll())
+                .withRel(IanaLinkRelations.ITEM)
+                .toMono();
+
+        return Mono.zip(repository.findById(id), selfLink, aggregateLink)
+                .map(o -> EntityModel.of(o.getT1(), Links.of(o.getT2(), o.getT3())));
+    }
+
+    @PostMapping("/affordances/items")
+    public Mono<ResponseEntity<?>> addNewItem(@RequestBody Mono<EntityModel<Item>> item) {
+        return item
+                .map(EntityModel::getContent)
+                .flatMap(this.repository::save)
+                .map(Item::getId)
+                .flatMap(this::findOne)
+                .map(newModel -> ResponseEntity.created(newModel
+                        .getRequiredLink(IanaLinkRelations.SELF)
+                        .toUri()).body(newModel.getContent()));
+    }
+
+    @PutMapping("/affordances/items/{id}")
     public Mono<ResponseEntity<?>> updateItem(@RequestBody Mono<EntityModel<Item>> item,
-                                              @PathVariable String id
-                                              ){
+                                              @PathVariable String id) {
         return item
                 .map(EntityModel::getContent)
                 .map(content -> new Item(id, content.getName(),
@@ -108,24 +108,23 @@ public class AffordancesItemController {
                 .then(findOne(id))
                 .map(model -> ResponseEntity.noContent()
                         .location(model.getRequiredLink(IanaLinkRelations.SELF).toUri()).build());
-
     }
 
-    // tag::profile[]
+
     @GetMapping(value = "/affordances/items/profile", produces = MediaTypes.ALPS_JSON_VALUE)
     public Alps profile() {
-        return alps() //
-                .descriptor(Collections.singletonList(descriptor() //
-                        .id(Item.class.getSimpleName() + "-representation") //
-                        .descriptor( //
-                                Arrays.stream(Item.class.getDeclaredFields()) //
-                                        .map(field -> descriptor() //
-                                                .name(field.getName()) //
-                                                .type(Type.SEMANTIC) //
-                                                .build()) //
-                                        .collect(Collectors.toList())) //
-                        .build())) //
+        return alps()
+                .descriptor(Collections.singletonList(descriptor()
+                        .id(Item.class.getSimpleName() + "-representation")
+                        .descriptor(
+                                Arrays.stream(Item.class.getDeclaredFields())
+                                        .map(field -> descriptor()
+                                                .name(field.getName())
+                                                .type(Type.SEMANTIC)
+                                                .build())
+                                        .collect(Collectors.toList()))
+                        .build()))
                 .build();
     }
-    // end::profile[]
+
 }
--- src/test/java/me/jongwoo/springbootch1reactive/controller/AffordancesItemControllerTest.java
+++ src/test/java/me/jongwoo/springbootch1reactive/controller/AffordancesItemControllerTest.java
@@ -9,20 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 
-@WebFluxTest(controllers = HypermediaItemController.class)
+@WebFluxTest(controllers = AffordancesItemController.class)
 @AutoConfigureRestDocs
-class HypermediaItemControllerTest {
+class AffordancesItemControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
@@ -34,17 +33,15 @@ class HypermediaItemControllerTest {
     ItemRepository repository;
 
     @Test
-    public void findOneItem(){
+    public void findSingleItemAffordances(){
         when(repository.findById("item-1")).thenReturn(Mono.just(
                 new Item("item-1", "Alf alarm clock", "nothing I really need", 19.99)));
 
-        this.webTestClient.get().uri("/hypermedia/items/item-1")
+        this.webTestClient.get().uri("/affordances/items/item-1")
+                .accept(MediaTypes.HAL_FORMS_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .consumeWith(document("findOne-hypermedia", preprocessResponse(prettyPrint()),
-                        links(
-                                linkWithRel("self").description("이 `Item`에 대한 공식 링크"),
-                                linkWithRel("item").description("`Item` 목록 링크"))));
+                .consumeWith(document("single-item-affordances", preprocessResponse(prettyPrint())));
     }
 }
