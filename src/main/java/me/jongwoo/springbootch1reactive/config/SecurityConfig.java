909223c04495b43d395402b41e2b8e48b13db414
limjongwoo <whddn528@naver.com>
2021-08-20 15:18:32 +0900

    SecurityConfig CustomSecurityPolicy add

--- src/main/java/me/jongwoo/springbootch1reactive/config/SecurityConfig.java
+++ src/main/java/me/jongwoo/springbootch1reactive/config/SecurityConfig.java
@@ -6,13 +6,33 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
+
+    static final String USER = "USER";
+    static final String INVENTORY = "INVENTORY";
+
+    @Bean
+    public SecurityWebFilterChain myCustomSecurityPolicy(ServerHttpSecurity http){
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                    .pathMatchers(HttpMethod.POST, "/").hasRole(INVENTORY)
+                        .pathMatchers(HttpMethod.DELETE, "/**").hasRole(INVENTORY)
+                    .anyExchange().authenticated()
+                .and()
+                    .httpBasic()
+                .and()
+                        .formLogin())
+                .csrf().disable().build();
+    }
 
     @Bean
     public ReactiveUserDetailsService userDetailsService(UserRepository repository){
